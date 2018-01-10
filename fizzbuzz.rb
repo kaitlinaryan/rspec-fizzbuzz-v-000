f5db7c04484864b8c5014254cbb12341b991865b
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-01-10 20:58:26 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,11 @@
 def fizzbuzz(n)
-  if
-n % 3 == 0 && n % 5 == 0
+if n % 3 == 0 && n % 5 == 0
 "FizzBuzz"
 
-elseif
-n % 3 == 0
+elseif n % 3 == 0
 "Fizz"
 
-elseif
-n % 5 == 0
+elseif n % 5 == 0
 "Buzz"
+end
 end
